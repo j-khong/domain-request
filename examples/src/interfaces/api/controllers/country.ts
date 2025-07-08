0cdccc07a1773416d1483edfc48220bdbd4bad97
j-khong <37912855+j-khong@users.noreply.github.com>
2025-07-08 13:22:17 +0200

    Deleted ConfigurationFile.ts

--- examples/src/interfaces/api/controllers/country.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from '/deps/index.ts';
-import { domainFetch } from '@services/country/index.ts';
-import { fetch as genericFetch } from '../helpers.ts';
-
-export async function fetch(ctx: Context) {
-   return genericFetch(ctx, domainFetch);
-}
